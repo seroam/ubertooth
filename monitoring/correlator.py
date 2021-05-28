a875a805a1d513735448ae12313fd93efcdafc08
Severin Marti <severin.marti@ost.ch>
2021-05-28 11:49:01 +0200

    Continue correlation

--- monitoring/correlator.py
+++ monitoring/correlator.py
@@ -6,6 +6,9 @@ from sqlite3.dbapi2 import Timestamp
 import sys
 from os.path import isfile
 import bisect
+from collections import namedtuple
+from math import asin, sqrt, sin, cos, radians
+Coords = namedtuple("Coords", "lat lng")
 
 class Fingerprint:
     def __init__(self, row: tuple):
@@ -39,12 +42,35 @@ class BtleAdvFingerprint:
         self.antenna = antenna
         self.successors = list()
         self.is_successor = False
+        self.is_static = False
+        self.type = 0
 
-    def get_chain(self, *, indent: int=0):
+    def get_chain(self, *, indent: int=0) -> str:
         return f'{" "*indent}{self.mac}\n'+ \
             '\n'.join(successor.get_chain(indent=indent+2) for successor in self.successors)
 
-    def __str__(self):
+    def add_candidates(self, candidates: list, max_candidates: int=2) -> None:
+        '''Filters, sorts and adds a list of candidates to the successors list\n
+        If candidates contains only one candidate, adds the list directly.\n
+        If candidates contains at most max_candidates candidates, sorts them by rssi difference and adds them to the list.\n
+        If candidates contains more than max_candidates candidates, appends the max_candidates candidates with the smallest rssi difference.\n
+        is_successor is only set on a Fingerprint if it is the only candidate.
+        '''
+        if (num_candidates := len(candidates)) == 0:
+            pass
+        elif num_candidates == 1:
+            self.successors = candidates
+            candidates[0].is_successor = True
+        else:
+            candidates.sort(key=lambda x: max(abs(self.rssi-x.mean)-x.std, 0))
+            self.successors = candidates[:max_candidates]
+
+    def is_possible_successor(self, candidate) -> bool:
+        #TODO: Compare type
+        #TODO: Compare location
+        return True
+
+    def __str__(self) -> str:
         return ', '.join(f'{k}={v}' for k, v in self.__dict__.items())
 
     def __lt__(self, other) -> bool:
@@ -96,20 +122,7 @@ class DbReader:
 
             return [BtleAdvFingerprint(*row[1:]) for row in rows]
 
-def find_successor(last_seen_old, rows, successors = None, indent=2):
-    
-    candidates = filter(lambda x: last_seen_old+5 >= x[5] >= last_seen_old, rows)
-    for candidate in candidates:
 
-        print(f'{" "*indent}{candidate[1]}: {candidate[5]}-{candidate[6]} ({candidate[6]-candidate[5]}s) on {candidate[7]}')
-        if successors is not None:
-            successors.append(candidate[1])
-
-        find_successor(candidate[6], rows, successors, indent+2)
-
-def usage():
-    print(f"Usage: {sys.argv[0]} path_to_db_file")
-    sys.exit(0)
 
 def process_btle_adv(*, delta_max: int=5, max_candidates: int=2):
     fingerprints = DbReader.get_mac_rows()
@@ -117,30 +130,43 @@ def process_btle_adv(*, delta_max: int=5, max_candidates: int=2):
     length = len(fingerprints)
 
     for index, fingerprint in enumerate(fingerprints):
+        if fingerprint.is_static:
+            continue
+
         first_candidate_index = bisect.bisect_left(fingerprints, fingerprint, index)
 
         candidates = list()
 
         if first_candidate_index < length:
             for candidate in fingerprints[first_candidate_index:]:
-                if candidate.first_seen - fingerprint.last_seen < 5:
-                    candidates.append(candidate)
+                if candidate.first_seen - fingerprint.last_seen < 5 and \
+                   fingerprint.is_possible_successor(candidate):
+                        candidates.append(candidate)
                 else:
                     break
 
-        if (num_candidates := len(candidates)) == 0 or num_candidates > max_candidates:
-            pass
-        elif (num_candidates := len(candidates)) == 1:
-            fingerprint.successors = candidates
-            candidates[0].is_successor = True
-        else:
-            fingerprint.successors = candidates
+        fingerprint.add_candidates(candidates)
             
     for fingerprint in fingerprints:
         if not fingerprint.is_successor:
             print(fingerprint.get_chain())
-            
 
+def haversine(a: tuple, b: tuple):
+    '''Haversine equations to calculate distance on sphere'''
+    earth_radius = 6371.0088
+
+    lat1, lng1 = a
+    lat2, lng2 = b
+
+    lat1, lng1, lat2, lng2 = radians(lat1), radians(lng1), radians(lat2), radians(lng2)
+
+    d = sin((lat2 - lat1) * 0.5) ** 2 + cos(lat1) * cos(lat2) * sin((lng2 - lng1) * 0.5) ** 2
+
+    return 2 * earth_radius * asin(sqrt(d))
+
+def usage():
+    print(f"Usage: {sys.argv[0]} path_to_db_file")
+    sys.exit(0)
 
 if __name__ == '__main__':
 
@@ -160,11 +186,6 @@ if __name__ == '__main__':
 
     DbReader(db_file)
 
-    DbReader.get_instance().get_antenna_path()
-    DbReader.get_instance().get_antenna_path(start=1)
-    DbReader.get_instance().get_antenna_path(start=1, end=2)
-
     process_btle_adv()
-
     
 
